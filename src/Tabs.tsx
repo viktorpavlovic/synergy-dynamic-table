d0ca5478a3d50129bb287d3812f410cde7d9508d
Viktor Pavlovic <viktor.pavlovic@factoryww.com>
2024-11-15 15:21:44 +0100

    first-commit

---
+++ src/Tabs.tsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+import HomeTable from "./HomeTable";
+import AwayTable from "./AwayTable";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
+export default function SynergyTable() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs value={value} onChange={handleChange}>
+          <Tab label="Konstantin" {...a11yProps(0)} />
+          <Tab label="Viktor" {...a11yProps(1)} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+        <HomeTable />
+        <AwayTable />
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        <HomeTable />
+        <AwayTable />
+      </CustomTabPanel>
+    </Box>
+  );
+}
